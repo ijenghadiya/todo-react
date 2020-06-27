e975546879886dcfe444d67ffb1915905235d7dc
Ijen <ghadiyaijen@gmail.com>
2020-06-27 12:03:17 +0530

    added edit,delete,filter functionalities

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
-
-
+const FILTER_MAP={
+  All:()=>true,
+  Active:task=>!task.completed,
+  Completed:task=>task.completed
+}
+const FILTER_NAMES=Object.keys(FILTER_MAP);
 function App(props) {
  
   const [tasks, setTasks]=useState(props.tasks);
@@ -26,14 +30,34 @@ function App(props) {
     const remainingTasks=tasks.filter(task=>id!==task.id);
     setTasks(remainingTasks);
   }
-  const taskList=tasks.map(task=>(
+  function editTask(id, newName){
+    const editedTaskList=tasks.map(task=>{
+      if (id===task.id){
+        return{...task, name:newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+  const[filter, setFilter]=useState('All');
+  const taskList=tasks
+  .filter(FILTER_MAP[filter])
+  .map(task=>(
   <Todo 
     id={task.id} 
     name={task.name} 
     completed={task.completed} 
     key={task.id}
     toggleTaskCompleted={toggleTaskCompleted}
-    deleteTask={deleteTask} 
+    deleteTask={deleteTask}
+    editTask={editTask} 
+  />));
+  const filterList=FILTER_NAMES.map(name=>(
+  <FilterButton 
+    key={name} 
+    name={name}
+    isPressed={name===filter}
+    setFilter={setFilter}
   />));
   const  tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const  headingText = `${taskList.length} ${tasksNoun} remaining`;
@@ -42,9 +66,7 @@ function App(props) {
       <h1>TodoMatic</h1>
      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+       {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
